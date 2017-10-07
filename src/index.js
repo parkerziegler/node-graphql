bbdae1541fca366c61e920ae6bc1fe80ec0c5f7e
INTEGRALGIS\pziegler <parker@integralgis.com>
2017-10-06 17:15:50 -0700

    Creating connection to Mongo and injecting  it into GraphQL context.

--- src/index.js
+++ src/index.js
@@ -9,16 +9,36 @@ const bodyParser = require('body-parser');
 // also import graphiqlExpress, it renders a nice IDE for testing the graphql server
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 
+// require our graphQL schema
 const schema = require('./schema');
 
-var app = express();
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+// require our db connection
+const connectMongo = require('./mongo-connector');
 
-app.use('/graphiql', graphiqlExpress({
-    endpointURL: '/graphql'
-}));
+// function to call when starting the server
+const start = async () => {
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Hackernews GraphQL server running on port ${PORT}.`);
-});
+    const mongo = await connectMongo();
+
+    // place the mongo connection into the graphQL context object
+    // context is a graphQL object that gets passed to all resolvers,
+    // good place to store db credentials, tokens, etc.
+    var app = express();
+    app.use('/graphql', bodyParser.json(), graphqlExpress({ 
+        context: { mongo },
+        schema
+    }));
+    
+    app.use('/graphiql', graphiqlExpress({
+        endpointURL: '/graphql'
+    }));
+    
+    const PORT = 3000;
+    app.listen(PORT, () => {
+        console.log(`Hackernews GraphQL server running on port ${PORT}.`);
+    });
+};
+
+// invoke the function
+start();
+
---
+++ src/mongo-connector.js
@@ -0,0 +1,12 @@
+// require the node mongo client
+const { MongoClient } = require('mongodb');
+
+// define the url we'll be connecting to mongo with
+const MONGO_URL = 'mongodb://localhost:27017/hackernews';
+
+// start the connection, use async await to wait for the
+// db connection to succeed
+module.exports = async () => {
+    const db = await MongoClient.connect(MONGO_URL);
+    return { Links: db.collection('links') };
+}
