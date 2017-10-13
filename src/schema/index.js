5d74d7275ed49b8c9388a66e7af92e27538fc107
parkerziegler <parkerellisziegler@gmail.com>
2017-10-12 17:32:59 -0700

    Updates to schema to accommodate adding users.

--- src/schema/index.js
+++ src/schema/index.js
@@ -11,12 +11,28 @@ const typeDefs = `
         description: String!
     }
 
+    type User {
+        id: ID!
+        name: String!
+        email: String
+    }
+
+    input AuthProviderSignupData {
+        email: AUTH_PROVIDER_EMAIL
+    }
+
+    input AUTH_PROVIDER_EMAIL {
+        email: String!
+        password: String!
+    }
+
     type Query {
         allLinks: [Link!]!
     }
 
     type Mutation {
         createLink(url: String!, description: String!): Link
+        createUser(name: String!, authProvider: AuthProviderSignupData!) : User
     }
 `;
 
