d104cf74b8055871af39cc2a7a132a0532b74b7a
parkerziegler <parkerellisziegler@gmail.com>
2017-10-12 17:33:15 -0700

    Resovler for creating a user.

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -18,10 +18,29 @@ const links = [
       },
     },
     Mutation: {
-      createLink: async (root, data, { mongo: {Links}}) => {
+
+      // our createLink resolver
+      createLink: async (root, data, { mongo: { Links }}) => {
         const response = await Links.insert(data);
         return Object.assign({ id: response.insertedIds[0] }, data);
       },
+
+      // our createUser resolver
+      createUser: async (root, data, { mongo: { Users }}) => {
+
+        // define a new user object
+        const newUser = {
+          name: data.name,
+          email: data.authProvider.email.email,
+          password: data.authProvider.email.password
+        };
+
+        // insert the new user
+        const response = await Users.insert(newUser);
+
+        // return the new user with the generated id
+        return Object.assign({id: response.insertedIds[0]}, newUser);
+      }
     },
     Link: {
       id: root => root._id || root.id,
