3c61d3455aedc4a0717b11c83686d389421ddd82
parkerziegler <parkerellisziegler@gmail.com>
2017-10-20 09:33:06 -0700

    Small formatting tweak.

--- src/authenticate.js
+++ src/authenticate.js
@@ -3,5 +3,5 @@ const HEADER_REGEX = /bearer token-(.*)$/;
 
 module.exports.authenticate = async ({ headers: { authorization }}, Users) => {
   const email = authorization && HEADER_REGEX.exec(authorization)[1];
-  return email && await Users.findOne({email});
+  return email && await Users.findOne({ email });
 }
