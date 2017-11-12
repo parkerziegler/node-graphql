0b9e23549099ae0f81ec541e754487ca034c168b
parkerziegler <parkerellisziegler@gmail.com>
2017-11-12 12:52:50 -0800

    Adding a temporary logger to monitor the number of requests made to the DB.

--- src/mongo-connector.js
+++ src/mongo-connector.js
@@ -1,5 +1,5 @@
 // require the node mongo client
-const { MongoClient } = require('mongodb');
+const { Logger, MongoClient } = require('mongodb');
 
 // define the url we'll be connecting to mongo with
 const MONGO_URL = 'mongodb://localhost:27017/hackernews';
@@ -8,6 +8,14 @@ const MONGO_URL = 'mongodb://localhost:27017/hackernews';
 // db connection to succeed
 module.exports = async () => {
     const db = await MongoClient.connect(MONGO_URL);
+
+    // some temporary logging
+    let logCount = 0;
+    Logger.setCurrentLogger((msg, state) => {
+      console.log(`MONGO DB REQUEST ${++logCount}: ${msg}`);
+    });
+    Logger.setLevel('debug');
+    Logger.filter('class', ['Cursor']);
 
     // return the mongodb collections for links, users, votes
     return {
