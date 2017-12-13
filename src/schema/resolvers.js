131b5cd05eac641457a9a9ba810cd9a423bfa4cb
parkerziegler <parkerellisziegler@gmail.com>
2017-12-12 17:12:41 -0800

    More robust error handling for validating links.

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -1,105 +1,127 @@
 const { ObjectID } = require('mongodb');
-  
-  module.exports = {
-    Query: {
-      allLinks: async (root, data, { mongo: { Links }}) => {
-        return await Links.find({}).toArray();
-      },
+const { URL } = require('url');
+
+// class for providing more specific error messages
+class ValidationError extends Error {
+
+  constructor(message, field) {
+    super(message);
+    this.field = field;
+  }
+}
+
+function assertValidLink({ url }) {
+  try {
+    new URL(url);
+  } catch (error) {
+    throw new ValidationError('Link validation error: invalid url.', 'url');
+  }
+}
+
+module.exports = {
+  Query: {
+    allLinks: async (root, data, { mongo: { Links }}) => {
+      return await Links.find({}).toArray();
     },
-    Mutation: {
+  },
+  Mutation: {
 
-      // our createLink resolver
-      createLink: async (root, data, { mongo: { Links }, user}) => {
-        // copy over the data sent in the resolver along with
-        // the user who posted it
-        const newLink = Object.assign({postedById: user && user._id}, data);
+    // our createLink resolver
+    createLink: async (root, data, { mongo: { Links }, user}) => {
 
-        // insert into our MongoDB databasse
-        const response = await Links.insert(newLink);
+      // check that the link is valid
+      assertValidLink(data);
 
-        // return the id of the newly created link
-        // in addition to the newLink object
-        return Object.assign({ id: response.insertedIds[0] }, newLink);
-      },
+      // copy over the data sent in the resolver along with
+      // the user who posted it
+      const newLink = Object.assign({postedById: user && user._id}, data);
 
-      // our createVote resolver
-      createVote: async (root, data, { mongo: { Votes }, user}) => {
+      // insert into our MongoDB databasse
+      const response = await Links.insert(newLink);
 
-        // compose the newVote object
-        const newVote = {
-          userId: user && user._id,
-          linkId: new ObjectID(data.linkId)
-        };
-
-        // insert it into mongo's votes collection
-        const response = await Votes.insert(newVote);
-
-        // return the id of the cast vote and it's contents
-        return Object.assign({ id: response.insertedIds[0] }, newVote);
-      },
-
-      // our createUser resolver
-      createUser: async (root, data, { mongo: { Users }}) => {
-
-        // define a new user object
-        const newUser = {
-          name: data.name,
-          email: data.authProvider.email.email,
-          password: data.authProvider.email.password
-        };
-
-        // insert the new user
-        const response = await Users.insert(newUser);
-
-        // return the new user with the generated id
-        return Object.assign({ id: response.insertedIds[0] }, newUser);
-      },
-
-      // our signin user resolver - take a look at the Users collection
-      // in the database - data is our auth credentials
-      signinUser: async (root, data, { mongo: { Users }}) => {
-        
-        // find the matching user
-        const user = await Users.findOne({ email: data.email.email });
-  
-        // check that the password matches
-        if (data.email.password === user.password) {
-          // if so, return a token
-          return { token: `token-${user.email}`, user };
-        }
-      }
+      // return the id of the newly created link
+      // in addition to the newLink object
+      return Object.assign({ id: response.insertedIds[0] }, newLink);
     },
-    Link: {
-      // Convert the "_id" field from MongoDB to "id" from the schema.
-      id: root => root._id || root.id,
 
-      postedBy: async ({ postedById }, data, {dataloaders: {userLoader}}) => {
-        return await userLoader.load(postedById);
-      },
+    // our createVote resolver
+    createVote: async (root, data, { mongo: { Votes }, user}) => {
 
-      votes: async ({ _id }, data, { mongo: { Votes }}) => {
-        return await Votes.find({ linkId: _id }).toArray();
-      }
+      // compose the newVote object
+      const newVote = {
+        userId: user && user._id,
+        linkId: new ObjectID(data.linkId)
+      };
+
+      // insert it into mongo's votes collection
+      const response = await Votes.insert(newVote);
+
+      // return the id of the cast vote and it's contents
+      return Object.assign({ id: response.insertedIds[0] }, newVote);
     },
-    User: {
-      // Convert the "_id" field from MongoDB to "id" from the schema.
-      id: root => root._id || root.id,
 
-      votes: async({ _id }, data, { mongo: Votes }) => {
-        return await Votes.find({ userId: _id }).toArray();
-      }
+    // our createUser resolver
+    createUser: async (root, data, { mongo: { Users }}) => {
+
+      // define a new user object
+      const newUser = {
+        name: data.name,
+        email: data.authProvider.email.email,
+        password: data.authProvider.email.password
+      };
+
+      // insert the new user
+      const response = await Users.insert(newUser);
+
+      // return the new user with the generated id
+      return Object.assign({ id: response.insertedIds[0] }, newUser);
     },
-    Vote: {
-      id: root => root._id || root.id,
 
-      // fetch the user data from mongo
-      user: async ({ userId }, data, { dataloaders: { userLoader }}) => {
-        return await userLoader.load(userId);
-      },
+    // our signin user resolver - take a look at the Users collection
+    // in the database - data is our auth credentials
+    signinUser: async (root, data, { mongo: { Users }}) => {
+      
+      // find the matching user
+      const user = await Users.findOne({ email: data.email.email });
 
-      // fetch the link data from mongo
-      link: async ({ linkId }, data, { mongo: { Links }}) => {
-        return await Links.findOne({ _id: linkId });
+      // check that the password matches
+      if (data.email.password === user.password) {
+        // if so, return a token
+        return { token: `token-${user.email}`, user };
       }
     }
-  };
+  },
+  Link: {
+    // Convert the "_id" field from MongoDB to "id" from the schema.
+    id: root => root._id || root.id,
+
+    postedBy: async ({ postedById }, data, {dataloaders: {userLoader}}) => {
+      return await userLoader.load(postedById);
+    },
+
+    votes: async ({ _id }, data, { mongo: { Votes }}) => {
+      return await Votes.find({ linkId: _id }).toArray();
+    }
+  },
+  User: {
+    // Convert the "_id" field from MongoDB to "id" from the schema.
+    id: root => root._id || root.id,
+
+    votes: async({ _id }, data, { mongo: Votes }) => {
+      return await Votes.find({ userId: _id }).toArray();
+    }
+  },
+  Vote: {
+    id: root => root._id || root.id,
+
+    // fetch the user data from mongo
+    user: async ({ userId }, data, { dataloaders: { userLoader }}) => {
+      return await userLoader.load(userId);
+    },
+
+    // fetch the link data from mongo
+    link: async ({ linkId }, data, { mongo: { Links }}) => {
+      return await Links.findOne({ _id: linkId });
+    }
+  }
+};
